a7630c2abab857caeaea093bdad74fed816d03b9
giscafer <giscafer@outlook.com>
2018-09-09 11:33:51 +0800

    修改配置说明

--- README.md
+++ README.md
@@ -13,6 +13,13 @@ make ocr more easy
 
 - support export into word
 
+
+## Usage
+
+- npm install
+- `.\module\config.js` 修改百度App ID、App KEY、SECRET KEY 为自己开发者应用配置
+- npm run start
+
 ## Dependencies
 
 
--- module/config.js
+++ module/config.js
@@ -1,3 +1,8 @@
+/**
+ * 修改百度App ID、App KEY、SECRET KEY 为自己开发者应用配置
+ * 以下配置为个人开发测试配置，可能会失效
+ */
+
 module.exports = {
     APP_ID: '10363783', // 你的 App ID
     API_KEY: 'VGD6eTCnbPYDRtX5YXYGweF7', // 你的 App KEY
